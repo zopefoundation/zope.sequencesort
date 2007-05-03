390fcb6560c47f000e1653c7febf16d6c1f7edad
Christian Theune <ct@gocept.com>
2007-05-03 22:31:01 +0000

    Removed external to move code here. Updated version for *upcoming* release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.sequencesort",
-    version="3.4dev",
+    version = '3.4.0b1',
     url='http://svn.zope.org/zope.sequencesort',
     license='ZPL 2.1',
     description='Zope sequencesort',
