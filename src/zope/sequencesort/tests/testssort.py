b20c0c81667cad35ae514c18e2eb7fe307377b86
Tres Seaver <tseaver@palladion.com>
2013-02-28 12:50:34 -0500

    Normalize tests.
    Remove '*' imports.
    
    Name testcase methods to indicate semantics.
    
    Don't import module-under-test at testcase module scope.

--- src/zope/sequencesort/tests/testssort.py
+++ src/zope/sequencesort/tests/testssort.py
@@ -10,55 +10,286 @@
 # FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
-
 import unittest
 
-from zope.sequencesort import ssort
-from zope.sequencesort.tests.ztestlib import *
-from zope.sequencesort.tests.results import *
 
-class TestCase(unittest.TestCase):
+class Test_sort(unittest.TestCase):
     """Test zope.sequencesort.sort()
     """
     def _callFUT(self, *args, **kw):
         from zope.sequencesort.ssort import sort
         return sort(*args, **kw)
 
-    def test1(self):
-        assert res1==ssort.sort(wordlist)
+    def test_wo_args(self):
+        self.assertEqual(self._callFUT(WORDLIST), RES_WO_ARGS)
 
-    def test2(self):
-        assert res2==ssort.sort(wordlist, (("key",),), mapping=1)
+    def test_w_only_key(self):
+        self.assertEqual(self._callFUT(WORDLIST, (("key",),), mapping=1),
+                         RES_W_ONLY_KEY)
 
-    def test3(self):
-        assert res3==ssort.sort(wordlist, (("key", "cmp"),), mapping=1)
+    def test_w_key_and_cmp(self):
+        self.assertEqual(self._callFUT(WORDLIST, (("key", "cmp"),), mapping=1),
+                         RES_W_KEY_AND_CMP)
 
-    def test4(self):
-        assert res4==ssort.sort(wordlist, (("key", "cmp", "desc"),),
-                                mapping=1)
+    def test_w_key_and_cmp_desc(self):
+        self.assertEqual(self._callFUT(WORDLIST, (("key", "cmp", "desc"),),
+                                       mapping=1), RES_W_KEY_AND_CMP_DESC)
 
-    def test5(self):
-        assert res5==ssort.sort(wordlist, (("weight",), ("key",)),
-                                mapping=1)
+    def test_w_multi_key(self):
+        self.assertEqual(self._callFUT(WORDLIST, (("weight",), ("key",)),
+                                       mapping=1), RES_W_MULTI_KEY)
 
-    def test6(self):
-        assert res6==ssort.sort(wordlist,
-                                (("weight",),
-                                 ("key", "nocase", "desc")),
-                                mapping=1)
+    def test_w_multi_key_nocase_desc(self):
+        self.assertEqual(self._callFUT(WORDLIST, (("weight",),
+                                       ("key", "nocase", "desc")), mapping=1),
+                         RES_W_MULTI_KEY_NOCASE_DESC)
 
-    def test7(self):
+    def test_w_custom_comparator(self):
         def myCmp(s1, s2):
             return -cmp(s1, s2)
 
         md = {"myCmp" : myCmp}
-        assert res7==ssort.sort(wordlist,
+        self.assertEqual(self._callFUT(WORDLIST,
                                 (("weight",), ("key", "myCmp", "desc")),
                                 md,
                                 mapping=1
-                                )
+                                ), RES_W_CUSTOM_COMPARATOR)
+
+
+WORDLIST = [
+   {"key": "aaa", "word": "AAA", "weight": 1},
+   {"key": "bbb", "word": "BBB", "weight": 0},
+   {"key": "ccc", "word": "CCC", "weight": 0},
+   {"key": "ddd", "word": "DDD", "weight": 0},
+   {"key": "eee", "word": "EEE", "weight": 1},
+   {"key": "fff", "word": "FFF", "weight": 0},
+   {"key": "ggg", "word": "GGG", "weight": 0},
+   {"key": "hhh", "word": "HHH", "weight": 0},
+   {"key": "iii", "word": "III", "weight": 1},
+   {"key": "jjj", "word": "JJJ", "weight": -1},
+   {"key": "kkk", "word": "KKK", "weight": 0},
+   {"key": "lll", "word": "LLL", "weight": 0},
+   {"key": "mmm", "word": "MMM", "weight": 0},
+   {"key": "nnn", "word": "NNN", "weight": 0},
+   {"key": "ooo", "word": "OOO", "weight": 1},
+   {"key": "ppp", "word": "PPP", "weight": 0},
+   {"key": "qqq", "word": "QQQ", "weight": -1},
+   {"key": "rrr", "word": "RRR", "weight": 0},
+   {"key": "sss", "word": "SSS", "weight": 0},
+   {"key": "ttt", "word": "TTT", "weight": 0},
+   {"key": "uuu", "word": "UUU", "weight": 1},
+   {"key": "vvv", "word": "VVV", "weight": 0},
+   {"key": "www", "word": "WWW", "weight": 0},
+   {"key": "xxx", "word": "XXX", "weight": 0},
+   {"key": "yyy", "word": "YYY", "weight": -1},
+   {"key": "zzz", "word": "ZZZ", "weight": 0}
+]
+
+RES_WO_ARGS = [
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+]
+
+RES_W_ONLY_KEY = [
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+]
+
+RES_W_KEY_AND_CMP = [
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+]
+
+RES_W_KEY_AND_CMP_DESC = [
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+]
+
+RES_W_MULTI_KEY = [
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+]
+
+RES_W_MULTI_KEY_NOCASE_DESC = [
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+]
+
+RES_W_CUSTOM_COMPARATOR = [
+    {'weight': -1, 'word': 'JJJ', 'key': 'jjj'},
+    {'weight': -1, 'word': 'QQQ', 'key': 'qqq'},
+    {'weight': -1, 'word': 'YYY', 'key': 'yyy'},
+    {'weight': 0, 'word': 'BBB', 'key': 'bbb'},
+    {'weight': 0, 'word': 'CCC', 'key': 'ccc'},
+    {'weight': 0, 'word': 'DDD', 'key': 'ddd'},
+    {'weight': 0, 'word': 'FFF', 'key': 'fff'},
+    {'weight': 0, 'word': 'GGG', 'key': 'ggg'},
+    {'weight': 0, 'word': 'HHH', 'key': 'hhh'},
+    {'weight': 0, 'word': 'KKK', 'key': 'kkk'},
+    {'weight': 0, 'word': 'LLL', 'key': 'lll'},
+    {'weight': 0, 'word': 'MMM', 'key': 'mmm'},
+    {'weight': 0, 'word': 'NNN', 'key': 'nnn'},
+    {'weight': 0, 'word': 'PPP', 'key': 'ppp'},
+    {'weight': 0, 'word': 'RRR', 'key': 'rrr'},
+    {'weight': 0, 'word': 'SSS', 'key': 'sss'},
+    {'weight': 0, 'word': 'TTT', 'key': 'ttt'},
+    {'weight': 0, 'word': 'VVV', 'key': 'vvv'},
+    {'weight': 0, 'word': 'WWW', 'key': 'www'},
+    {'weight': 0, 'word': 'XXX', 'key': 'xxx'},
+    {'weight': 0, 'word': 'ZZZ', 'key': 'zzz'},
+    {'weight': 1, 'word': 'AAA', 'key': 'aaa'},
+    {'weight': 1, 'word': 'EEE', 'key': 'eee'},
+    {'weight': 1, 'word': 'III', 'key': 'iii'},
+    {'weight': 1, 'word': 'OOO', 'key': 'ooo'},
+    {'weight': 1, 'word': 'UUU', 'key': 'uuu'},
+]
+
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestCase))
-    return suite
+    return unittest.TestSuite((
+        unittest.makeSuite(Test_sort),
+    ))
