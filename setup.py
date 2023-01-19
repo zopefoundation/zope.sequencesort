f4d17ff6ea194af217bd8352bac412a8c1d90c6f
Michael Howitz <mh@gocept.com>
2023-01-19 08:55:12 +0100

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 setup(
     name="zope.sequencesort",
-    version='5.0',
+    version='5.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Sequence Sorting',
